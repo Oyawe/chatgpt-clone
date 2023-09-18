429d103af206eceb81765aac8d9f3668e59f5cf9
Oyawe <oyawegoodness@gmail.com>
2023-09-18 16:56:55 -0700

    UI design completed successfully

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import msg from './assets/message.svg'
 import home from './assets/home.svg'
 import saved from './assets/bookmark.svg'
 import rocket from './assets/rocket.svg'
+import sendbtn from './assets/send.svg'
+import user from './assets/user-icon.png'
+import gptImgLogo from './assets/chatgptLogo.svg'
 
 function App() {
   return (
@@ -37,7 +40,21 @@ function App() {
       </div>
 
       <div className="main">
-
+        <div className="chats">
+          <div className="chat">
+            <img className='chat-img' src={user} alt="" /><p className='txt'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis recusandae labore accusamus quia eaque aliquam illum dignissimos doloremque voluptatum rem.</p>
+          </div>
+          <div className="chat bot">
+            <img className='chat-img' src={gptImgLogo} alt="" /><p className='txt'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et natus tempore doloribus tempora velit? Temporibus eum molestias aliquid minima obcaecati, accusamus ea reprehenderit soluta enim cumque distinctio ad praesentium voluptas error delectus! Numquam, sunt voluptatum! Iure neque ex enim tenetur suscipit id animi facere tempore, modi at adipisci, sed odit laudantium. Nostrum ullam deserunt autem velit commodi omnis perferendis alias dicta totam quam! Quod fuga rem, dolor impedit hic commodi doloremque reiciendis quis nemo. Debitis deserunt, excepturi autem voluptatibus voluptatem cupiditate provident quod in, beatae nostrum ad ab quidem dignissimos at laboriosam molestiae necessitatibus, est quam possimus eum aliquid? Doloremque.</p>
+          </div>
+        </div>
+        <div className="chat-footer">
+          <div className="inp">
+            <input type="text" placeholder="Type a message" />
+            <button className="send"><img src={sendbtn} alt="send" /></button>
+          </div>
+          <p>ChatGPT may produce inaccurate information about people, places or facts. ChatGPT August 20 Version</p>
+        </div>
       </div>
     </div>
   );
