1459313085a73ab9facdf98e90f30a3c48cf5750
Oyawe <oyawegoodness@gmail.com>
2023-09-18 11:42:59 -0700

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg'
+import addbtn from './assets/add-30.png'
+import msg from './assets/message.svg'
+import home from './assets/home.svg'
+import saved from './assets/bookmark.svg'
+import rocket from './assets/rocket.svg'
 
 function App() {
   return (
     <div className="App">
+      <div className="sidebar">
+        <div className="upperside">
+          <div className="upper-side-top">
+            <div className='logo-wrapper'><img src={gptLogo} alt="logo" className='logo' />
+              <span className="brand">ChatGpt</span>
+            </div>
+            <button className="mid-btn"><img src={addbtn} alt="new chat" className="add-btn" />New Chat</button>
+            <div className="upper-side-bottom">
+              <button className="query"><img src={msg} alt="icon" />What is Programming ?</button>
+              <button className="query"><img src={msg} alt="icon" />How to use an API ?</button>
+            </div>
+          </div>
+        </div>
 
+        <div className="lowerside">
+          <div className="listItems">
+            <img src={home} alt="home" className="list-items-img" />Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="saved" className="list-items-img" />Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="rocket" className="list-items-img" />Upgrade to Pro
+          </div>
+        </div>
+      </div>
+
+      <div className="main">
+
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 
-// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
